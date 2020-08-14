6a949d6bbe8938e5261abad16e4cb93a2b8eae13
Anton Skryba <antonskr92@gmail.com>
2020-08-14 15:29:51 +0300

    Initial Commit

---
+++ CoctailsRecipes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoctailsRecipes
+//
+//  Created by s.k.a.i on 14/08/2020.
+//  Copyright © 2020 s.k.a.i. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
